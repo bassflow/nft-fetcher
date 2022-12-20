047c99d790816ff37434a8ba0e4ff498e88eaeac
Bart Pekelaer <81241027+bartpekelaer@users.noreply.github.com>
2022-12-20 21:58:22 +0100

    added docstrings

--- const.py
+++ const.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import datetime
 import json
 
@@ -8,8 +10,6 @@ TXNURL = "https://api.snowtrace.io/api?module=account&action=tokennfttx&address=
 ADDRESSURL = "https://snowtrace.io/address/"
 ABIURL = "https://api.snowtrace.io/api?module=contract&action=getabi&address="
 CWDPATH = "C:\Program Files (x86)\chromedriver.exe"
-DB_PATH = "./data/db/databse.db"
-BUILD_PATH = "./data/db/build.sql"
 URL_REGEX = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
 READCONTRACT = "#readContract"
 WRITECONTRACT = "#writeContract"
@@ -17,6 +17,10 @@ CODECONTRACT = "#code"
 
 
 def timestamp_to_time_and_date(timestamp):
+    """
+    This Function converts a timestamp to a readable format:
+    e.g. March 31, 2022, 01:20 PM
+    """
     dt = datetime.datetime.fromtimestamp(timestamp)
 
     date = dt.strftime("%B %d, %Y")
@@ -26,6 +30,9 @@ def timestamp_to_time_and_date(timestamp):
 
 
 def load_json(filename):
+    """
+    This Function loads a json file, take the filename as argument.
+    """
     with open(filename, "r") as f:
         data = json.load(f)
 
@@ -33,5 +40,8 @@ def load_json(filename):
 
 
 def dump_json(filename, arg):
+    """
+    This function dumps data into a json file, taking filename and and the list to dump as arguments.
+    """
     with open(filename, "w") as of:
         json.dump(arg, of, indent=4)
--- contract.py
+++ contract.py
@@ -1,37 +1,12 @@
+#!/usr/bin/env python3
+
 import json
 import time
 import requests
 import const
-from selenium import webdriver
 
 
-contract = "0xcf0f4519f2f55ef40e2dddcb7c99893297e40336"
-driver = webdriver.Chrome(const.CWDPATH)
-search = driver.find_element("form-control form-control-xs")
+# contract = "0xcf0f4519f2f55ef40e2dddcb7c99893297e40336"
 
-
-def launchBrowser():
-    driver.get(const.ADDRESSURL + contract + const.READCONTRACT)
-    while True:
-        pass
-
-
-launchBrowser()
-
-
-def enterTokenID():
-    search.send_keys("test")
-    search.send_keys(Keys.RETURN)
-    while True:
-        pass
-
-
-enterTokenID()
-# nftcontracts = open("data/nfttx.json", "r")
-# contract = json.load(nftcontracts["contract_address"])
-
-
-# def get_erc721_smartcontract(contract):
-#    url = const.ABIURL + addres
-#    reponse = requests.request("GET", url)
-#    return reponse.json()["result"]
+nftcontracts = open("data/nfttx.json", "r")
+contract = json.load(nftcontracts)
